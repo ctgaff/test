19ae608a7cf178c2fcb990b298406b70e152d399
Corey Gaffney <ctgaff@gmail.com>
2026-08-23 19:40:45 -0500

    added new tag to trigger autodeploy in spacelift

--- main.tf
+++ main.tf
@@ -26,4 +26,12 @@ resource "aws_s3_bucket" "orbit_storage" {
     mission   = "First Launch"
     project   = "Orbit-labs"
   }
+
+  tags = {
+    name        = "Orbit Labs Storage"
+    managedBy   = "Spacelift"
+    mission     = "First Launch"
+    project     = "Orbit-labs"
+    environment = "demo"
+  }
 }
