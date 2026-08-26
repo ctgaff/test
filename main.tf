4ba3bc1f3bdf94f06eac99cfd9c7b1614519d5ea
Corey Gaffney <ctgaff@gmail.com>
2026-08-25 22:24:54 -0500

    Configure app stack to consume the input

--- main.tf
+++ main.tf
@@ -7,11 +7,36 @@ terraform {
   }
 }
 
+variable "subnet_id" {
+  type      = string
+  description = "ID of the subnet from networking stack"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 data "aws_caller_identity" "current" {}
+
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+data "aws_vpc" "selected" {
+  id = data.aws_subnet.selected.vpc_id
+}
+
+resource "aws_security_group" "app" {
+  name      = "orbit-labs-app-sg"
+  description = "Security group for Orbit Labs app"
+  vpc_id    = data.aws_vpc.selected.id
+
+  tags = {
+    name    = "Orbit Labs App SG"
+    project = "Orbit-labs"
+  }
+}
+
 
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
