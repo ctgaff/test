a97029dc99f87a22f8b7d3ba39945826d10d64ce
Corey Gaffney <ctgaff@gmail.com>
2026-09-06 13:21:26 -0500

    New main.tf for Learning Demo

--- main.tf
+++ main.tf
@@ -1,55 +1,21 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-variable "subnet_id" {
-  type      = string
-  description = "ID of the subnet from networking stack"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
-
-data "aws_subnet" "selected" {
-  id = var.subnet_id
+variable "environment" {
+  default = "demo"
 }
 
-data "aws_vpc" "selected" {
-  id = data.aws_subnet.selected.vpc_id
-}
-
-resource "aws_security_group" "app" {
-  name      = "orbit-labs-app-sg"
-  description = "Security group for Orbit Labs app"
-  vpc_id    = data.aws_vpc.selected.id
-
-  tags = {
-    name    = "Orbit Labs App SG"
-    project = "Orbit-labs"
-  }
-}
-
-
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-resource "aws_s3_bucket" "orbit_storage" {
-  bucket_prefix = "orbit-storage-"
+resource "aws_s3_bucket" "data" {
+  bucket_prefix = "orbit-labs-"
 
   tags = {
     name        = "Orbit Labs Storage"
     managedBy   = "Spacelift"
-    mission     = "First Launch"
-    project     = "Orbit-labs"
-    environment = "demo"
+    environment = var.environment
   }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.data.id
 }
